02b9b54ed21ef706c628ee700ebbd2d5933347d0
Ksenia <ks.borisova2017@gmail.com>
2025-04-03 20:38:22 +0300

    добавлены новые поля, изменен шаблон

--- Letter_generator/Controllers/LetterDataController.cs
+++ Letter_generator/Controllers/LetterDataController.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using System;
 using System.IO;
-using Microsoft.AspNetCore.Mvc;
+
 using DocumentFormat.OpenXml.Packaging;
 using DocumentFormat.OpenXml.Wordprocessing;
 namespace Letter_generator.Controllers
@@ -16,7 +16,7 @@ namespace Letter_generator.Controllers
         {
             if (data == null) return BadRequest("Нет данных");
 
-            string templatePath = "template.docx";
+            string templatePath = "template1.docx";
             if (!System.IO.File.Exists(templatePath))
             {
                 return NotFound("Шаблон не найден");
@@ -30,9 +30,18 @@ namespace Letter_generator.Controllers
                 foreach (var text in body.Descendants<Text>())
                 {
                     text.Text = text.Text
-                        .Replace("{NAME}", data.Name)
-                        .Replace("{DATE}", data.Date)
-                        .Replace("{MESSAGE}", data.Message);
+                        .Replace("{CurrentLettNumb}", data.CurrentLettNumb)
+                        .Replace("{CurrentDate}", data.CurrentDate)
+                        .Replace("{IncomingLettNumb}", data.IncomingLettNumb)
+                        .Replace("{IncommingDate}", data.IncommingDate)
+                        .Replace("{Post}", data.Post)
+                        .Replace("{Organization}", data.Organization)
+                        .Replace("{Sender_FIO}", data.Sender_FIO)
+                        .Replace("{Theme}", data.Theme)
+                        .Replace("{Text}", data.Text)
+                        .Replace("{Performer_FI}", data.Performer_FIO)
+                        .Replace("{Performer_phone}", data.Performer_phone)
+                        .Replace("{Performer_email}", data.Performer_email);
                 }
                 doc.MainDocumentPart.Document.Save();
             }
--- Letter_generator/Models/LetterData.cs
+++ Letter_generator/Models/LetterData.cs
@@ -2,8 +2,19 @@
 {
     public class LetterData
     {
-        public string Name { get; set; }
-        public string Date { get; set; }
-        public string Message { get; set; }
-    }
+        public string CurrentLettNumb { get; set; }
+        public string CurrentDate { get; set; }
+        public string IncomingLettNumb { get; set; }
+        public string IncommingDate { get; set; }
+        public string Post { get; set; }
+        public string Organization { get; set; }
+        public string Sender_FIO { get; set; }
+        public string Theme { get; set; }
+        public string Text { get; set; }
+        public string Performer_FIO { get; set; }
+        public string Performer_phone { get; set; }
+        public string Performer_email { get; set; }
+
+
+    }   
 }
